8f6aabbc1feddd0b4dec7710ba07586cd8446e08
Aleixein <megasaleixein@gmail.com>
2016-08-16 15:40:34 -0500

    Update ejercicio_1.js

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,14 +1,13 @@
-var miCanvas1 = document.getElementById("miCanvas1");
-var contexto1 = miCanvas1.getContext1("2d");
+var miCanvas = document.getElementById("miCanvas");
+var contexto1 = miCanvas.getContext1("2d");
+var contexto2 = miCanvas.getContext2("2d");
+var contexto3 = miCanvas.getContext3("2d");
+
 contexto1.fillStyle = "red";
 contexto1.fillRect(30,30,50,50);
 
-var miCanvas2 = document.getElementById("miCanvas2");
-var contexto2 = miCanvas2.getContext2("2d");
 contexto2.fillStyle = "green";
 contexto2.fillRect(130,30,50,25);
 
-var miCanvas3 = document.getElementById("miCanvas3");
-var contexto3 = miCanvas1.getContext3("2d");
 contexto3.fillStyle = "blue";
 contexto3.fillRect(30,130,25,50);
