242af350e81b9c3ec927cdb7714192bcd860ab7b
Aleixein <megasaleixein@gmail.com>
2016-08-22 18:07:11 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -4,8 +4,13 @@ camara.position.z = 5;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
-var forma = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshNormalMaterial();
-var cubo = new THREE.Mesh( forma, material ); 
-escena.add(cubo);
+var forma_tronco = new THREE.CylinderGeometry(2,2,5,20);
+var textura_madera = new THREE.TextureLoader().load( "textures/water.jpg" );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 4, 4 );
+var material_tronco = new SpriteMaterial(textura_madera);
+var tronco = new THREE.Mesh( forma_tronco, material_tronco ); 
+
+escena.add(tronco);
 renderizador.render(escena,camara);
