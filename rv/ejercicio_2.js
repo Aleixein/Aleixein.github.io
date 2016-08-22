be665eb86ffc82832674759cca617a7007811261
Aleixein <megasaleixein@gmail.com>
2016-08-22 18:20:09 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -1,11 +1,11 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 10;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
-var forma_tronco = new THREE.CylinderGeometry(2,2,5,20);
-var material_tronco = new THREE.MeshBasicMaterial( { color:0x683603, wireframe: true } );
+var forma_tronco = new THREE.CylinderGeometry(1,1,3,20);
+var material_tronco = new THREE.MeshBasicMaterial( { color:0x683603, wireframeLinewidth:2 } );
 var tronco = new THREE.Mesh( forma_tronco, material_tronco ); 
 
 escena.add(tronco);
