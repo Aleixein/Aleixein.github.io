c428373a8b87d1953c050bc1c5b21190caf75663
Aleixein <megasaleixein@gmail.com>
2016-08-22 18:18:22 -0500

    Update ejercicio_2.js

--- rv/ejercicio_2.js
+++ rv/ejercicio_2.js
@@ -5,8 +5,7 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 var forma_tronco = new THREE.CylinderGeometry(2,2,5,20);
-var map = new THREE.TextureLoader().load( "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTnQwwrY6MTVWD5CRvroaJaRQ2zUpcqqt9nUvdk7cZUBXSFLrFH" );
-var material_tronco = new THREE.MeshBasicMaterial( { map: map } );
+var material_tronco = new THREE.MeshBasicMaterial( { color:0x683603, wireframe: true } );
 var tronco = new THREE.Mesh( forma_tronco, material_tronco ); 
 
 escena.add(tronco);
