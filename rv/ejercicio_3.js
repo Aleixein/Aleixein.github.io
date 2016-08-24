a05fde535554e73321ece1cc3a02bd97489b94da
Aleixein <megasaleixein@gmail.com>
2016-08-24 18:12:17 -0500

    Update ejercicio_3.js

--- rv/ejercicio_3.js
+++ rv/ejercicio_3.js
@@ -9,7 +9,7 @@ forma.vertices.push( new THREE.Vector3( 1, 2, -1));
 forma.vertices.push( new THREE.Vector3(-1, 2, -1));
 forma.vertices.push( new THREE.Vector3( -1, 2, 1));
 
-forma.faces.push( new THREE.Face3( 3, 2, 1) );
+forma.faces.push( new THREE.Face3( 2, 1, 3) );
 forma.faces.push( new THREE.Face3( 3, 1, 0) );
 //forma.faces.push( new THREE.Face3( 3, 7, 4) );
 //forma.faces.push( new THREE.Face3( 3, 4, 0) );
