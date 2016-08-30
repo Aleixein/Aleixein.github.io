e877013c5e5d83190dbce5a3f47b9223552ab2c4
Aleixein <megasaleixein@gmail.com>
2016-08-30 15:24:40 -0500

    Update ejemplo-union.js

--- ejemplo-union.js
+++ ejemplo-union.js
@@ -11,7 +11,7 @@ arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 arbolForma.merge(esferaForma.geometry, esferaForma.matrix);
 
 var material = new THREE.MeshNormalMaterial();
-var arbolMalla = new THREE.Mesh(arbolForma, material);
+var arbolMalla = new THREE.Mesh( arbolForma, material );
 
 var escena = new THREE.Scene();
 escena.add(arbolMalla);
@@ -20,6 +20,6 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95)
-document.body.appendChild( renderizador.domElement );
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement ); 
 renderizador.render( escena, camara );
