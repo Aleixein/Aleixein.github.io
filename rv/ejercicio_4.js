20c8527186f672ca4b5bd6babbdf971acb6038bb
Aleixein <megasaleixein@gmail.com>
2016-09-01 15:28:50 -0500

    Update ejercicio_4.js

--- rv/ejercicio_4.js
+++ rv/ejercicio_4.js
@@ -19,7 +19,7 @@ var escena = new THREE.Scene();
 escena.add(mallaEstrella);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 150;
+camara.position.z = 160;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
