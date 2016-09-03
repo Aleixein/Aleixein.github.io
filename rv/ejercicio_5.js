631eb70f3d18fc286b0fc872ece59d2b6f5b8071
Aleixein <megasaleixein@gmail.com>
2016-09-03 02:25:49 -0500

    Update ejercicio_5.js

--- rv/ejercicio_5.js
+++ rv/ejercicio_5.js
@@ -4,10 +4,10 @@ var base3Forma = new THREE.CylinderGeometry( 4, 6, 1, 20);
 var base4Forma = new THREE.CylinderGeometry( 4, 4, 4, 20);
 var base5Forma = new THREE.CylinderGeometry( 6, 4, 1, 20);
 var cimaForma = new THREE.CylinderGeometry( 6, 6, 1, 20);
-var diente1Forma = new THREE.BoxGeometry( 1, 2, 0.75);
-var diente2Forma = new THREE.BoxGeometry( 1, 2, 0.75);
-var diente3Forma = new THREE.BoxGeometry( 1, 2, 0.75);
-var diente4Forma = new THREE.BoxGeometry( 1, 2, 0.75);
+var diente1Forma = new THREE.BoxGeometry( 1, 2, 1);
+var diente2Forma = new THREE.BoxGeometry( 1, 2, 1);
+var diente3Forma = new THREE.BoxGeometry( 1, 2, 1);
+var diente4Forma = new THREE.BoxGeometry( 1, 2, 1);
 
 base2Forma.translate(0, 1, 0);
 base3Forma.translate(0, 2, 0);
