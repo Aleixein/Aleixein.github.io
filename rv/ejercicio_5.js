67e9a2b8d6d63905f472129c4f371ff6c99a7427
Aleixein <megasaleixein@gmail.com>
2016-09-03 01:34:46 -0500

    Update ejercicio_5.js

--- rv/ejercicio_5.js
+++ rv/ejercicio_5.js
@@ -3,17 +3,20 @@ var base2Forma = new THREE.CylinderGeometry( 6, 6, 1, 20);
 var base3Forma = new THREE.CylinderGeometry( 4, 6, 1, 20);
 var base4Forma = new THREE.CylinderGeometry( 4, 4, 4, 20);
 var base5Forma = new THREE.CylinderGeometry( 6, 4, 1, 20);
+var cimaForma = new THREE.CylinderGeometry( 6, 6, 1, 20);
 
 base2Forma.translate(0, 1, 0);
 base3Forma.translate(0, 2, 0);
 base4Forma.translate(0, 4.5, 0);
 base5Forma.translate(0, 7, 0);
+cimaForma.translate(0, 8, 0);
 
 var base1Malla = new THREE.Mesh(base1Forma);
 var base2Malla = new THREE.Mesh(base2Forma);
 var base3Malla = new THREE.Mesh(base3Forma);
 var base4Malla = new THREE.Mesh(base4Forma);
 var base5Malla = new THREE.Mesh(base5Forma);
+var cimaMalla = new THREE.Mesh(cimaForma);
 
 var torreForma = new THREE.Geometry();
 
@@ -22,6 +25,7 @@ torreForma.merge(base2Malla.geometry, base2Malla.matrix);
 torreForma.merge(base3Malla.geometry, base3Malla.matrix);
 torreForma.merge(base4Malla.geometry, base4Malla.matrix);
 torreForma.merge(base5Malla.geometry, base5Malla.matrix);
+torreForma.merge( cimaMalla.geometry, cimaMalla.matrix );
 
 var material = new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh( torreForma, material );
