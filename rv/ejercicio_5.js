0ff812d8b4f82ebd200091088de8d98d61291a11
Aleixein <megasaleixein@gmail.com>
2016-09-03 02:34:05 -0500

    Update ejercicio_5.js

--- rv/ejercicio_5.js
+++ rv/ejercicio_5.js
@@ -4,17 +4,20 @@ var base3Forma = new THREE.CylinderGeometry( 2, 3, 1, 20);
 var base4Forma = new THREE.CylinderGeometry( 2, 2, 4, 20);
 var base5Forma = new THREE.CylinderGeometry( 3, 2, 1, 20);
 var cimaForma = new THREE.CylinderGeometry( 3, 3, 1, 20);
-var diente1Forma = new THREE.BoxGeometry( 1, 2, 1);
-var diente2Forma = new THREE.BoxGeometry( 1, 2, 1);
-var diente3Forma = new THREE.BoxGeometry( 1, 2, 1);
-var diente4Forma = new THREE.BoxGeometry( 1, 2, 1);
+var diente1Forma = new THREE.BoxGeometry( 1, 1.5, 1);
+var diente2Forma = new THREE.BoxGeometry( 1, 1.5, 1);
+var diente3Forma = new THREE.BoxGeometry( 1, 1.5, 1);
+var diente4Forma = new THREE.BoxGeometry( 1, 1.5, 1);
 
 base2Forma.translate(0, 1, 0);
 base3Forma.translate(0, 2, 0);
 base4Forma.translate(0, 4.5, 0);
 base5Forma.translate(0, 7, 0);
 cimaForma.translate(0, 8, 0);
-diente1Forma.translate(0, 10, 3);
+diente1Forma.translate( 0, 10, 2.9);
+diente2Forma.translate(0, 10, -2.9);
+diente3Forma.translate( 2.9, 10, 0);
+diente4Forma.translate(-2.9, 10, 0);
 
 var base1Malla = new THREE.Mesh(base1Forma);
 var base2Malla = new THREE.Mesh(base2Forma);
@@ -23,6 +26,9 @@ var base4Malla = new THREE.Mesh(base4Forma);
 var base5Malla = new THREE.Mesh(base5Forma);
 var cimaMalla = new THREE.Mesh(cimaForma);
 var diente1Malla = new THREE.Mesh(diente1Forma);
+var diente2Malla = new THREE.Mesh(diente2Forma);
+var diente3Malla = new THREE.Mesh(diente3Forma);
+var diente4Malla = new THREE.Mesh(diente4Forma);
 
 var torreForma = new THREE.Geometry();
 
@@ -33,6 +39,9 @@ torreForma.merge(base4Malla.geometry, base4Malla.matrix);
 torreForma.merge(base5Malla.geometry, base5Malla.matrix);
 torreForma.merge( cimaMalla.geometry, cimaMalla.matrix );
 torreForma.merge(diente1Malla.geometry, diente1Malla.matrix);
+torreForma.merge(diente2Malla.geometry, diente2Malla.matrix);
+torreForma.merge(diente3Malla.geometry, diente3Malla.matrix);
+torreForma.merge(diente4Malla.geometry, diente4Malla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var torreMalla = new THREE.Mesh( torreForma, material );
