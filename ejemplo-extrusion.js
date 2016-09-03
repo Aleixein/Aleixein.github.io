d31079c3e39ec96f6491f773440fde0d8dd61257
Aleixein <megasaleixein@gmail.com>
2016-09-03 02:16:05 -0500

    Update ejemplo-extrusion.js

--- ejemplo-extrusion.js
+++ ejemplo-extrusion.js
@@ -1,21 +1,22 @@
-var figura = new THREE.Shape(); 
+var diente1 = new THREE.Shape(); 
 
-figura.moveTo(10,10);
-figura.lineTo(10,40);
-figura.lineTo(40,40);
-figura.lineTo(10,10);
+diente1.moveTo( 0, 9 );
+diente1.lineTo( 1, 9 );
+diente1.lineTo( 1, 10);
+diente1.lineTo( 0, 10);
+diente1.lineTo( 0, 9 );
 
-var forma = new THREE.ExtrudeGeometry( figura, {amount: 10} );
+var diente1Forma = new THREE.ExtrudeGeometry( diente1, {amount: 0.5} );
 
 var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( forma, material );
+var malla = new THREE.Mesh( diente1Forma, material );
 malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 500;
+camara.position.z = 35;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95 );
