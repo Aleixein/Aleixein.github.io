a929f5e16acdaa5f9ed86dd9b8d56cca64aa5c7f
Aleixein <megasaleixein@gmail.com>
2016-09-08 15:31:20 -0500

    Update ejemplo-ortografica.js

--- ejemplo-ortografica.js
+++ ejemplo-ortografica.js
@@ -1,4 +1,4 @@
-var camara = new THREE.OrtographicCamera();
+var camara = new THREE.OrthographicCamera();
 
 camara.left = window.innerWidth/-2;
 camara.right = window.innerWidth/2;
@@ -11,14 +11,14 @@ camara.updateProjectionMatrix(); //Actualiza la matriz de proyección
 camara.position.z = 100;
 
 var cubo = new THREE.Mesh( new THREE.BoxGeometry(30,30,30), new THREE.MeshNormalMaterial() );
-cubo.rotate.y(Math.PI/4);
+cubo.rotateY(Math.PI/4);
 
 var esfera1 = new THREE.Mesh( new THREE.SphereGeometry(15), new THREE.MeshNormalMaterial() );
 esfera1.position.x = 50;
 
 var esfera2 = new THREE.Mesh( new THREE.SphereGeometry(15), new THREE.MeshNormalMaterial() );
 esfera2.position.x = -50;
-esfera2.position.y = -100;
+esfera2.position.z = -100;
 
 var escena = new THREE.Scene();
 escena.add(esfera1);
