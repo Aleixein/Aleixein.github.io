069562573092f3b451193480ec40da53f0fb7e20
Aleixein <megasaleixein@gmail.com>
2016-09-09 13:56:23 -0500

    Update ejercicio_6_tablero.js

--- rv/ejercicio_6_tablero.js
+++ rv/ejercicio_6_tablero.js
@@ -10,7 +10,7 @@ for (var i = 0; i < 8; i++ ) {
   for ( var j = 0; j < 8; j++ ) {
     if ( (i+j) % 2 == 0){
       var cuboMalla = new THREE.Mesh( cuboForma,colorBlanco );
-      cuaros.push ( cuboMalla );
+      cuadros.push ( cuboMalla );
     }
     else{
       var cuboMalla = new THREE.Mesh( cuboForma,colorGris );
