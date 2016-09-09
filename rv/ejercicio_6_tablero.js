01f0d6eb4f012441944079b561aaa9c0054bf040
Aleixein <megasaleixein@gmail.com>
2016-09-09 13:38:34 -0500

    Update ejercicio_6_tablero.js

--- rv/ejercicio_6_tablero.js
+++ rv/ejercicio_6_tablero.js
@@ -1,12 +1,20 @@
 car camara = new THREE.PerspectiveCamera();
 
 var cuboForma = new THREE.BoxGeometry(10,10,10)
-var gris = new THREE.MeshBasicMaterial({color: 0xcfcfcf});
-var blanco = new THREE.MeshBasicMaterial({color: 0xffffff});
+var colorGris = new THREE.MeshBasicMaterial({color: 0xcfcfcf});
+var colorBlanco = new THREE.MeshBasicMaterial({color: 0xffffff});
 
 for (var i = 0; i < 8; i++ ) {
   for ( var j = 0; j < 8; j++ ) {
-    var cuboMalla = new THREE.Mesh(cubo,
+    if ( (i+j) % 2 == 0){
+      var cuboMalla = new THREE.Mesh( cuboForma,colorBlanco );
+    }
+    else{
+      var cuboMalla = new THREE.Mesh( cuboForma,colorGris );
+    }
+      position.cuboMalla.x = i*10;
+      position.cuboMalla.z = j*10;
+    }
   }
 }
 
