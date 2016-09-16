5f0637fb77f5432e93d285f875710c77c7fc1ef1
Aleixein <megasaleixein@gmail.com>
2016-09-16 13:26:16 -0500

    Update ejercicio_6_camaraPerspectiva.js

--- rv/ejercicio_6_camaraPerspectiva.js
+++ rv/ejercicio_6_camaraPerspectiva.js
@@ -1,11 +1,12 @@
-var campoVision = 45; //en grados
+var campoVision = 60; //en grados
 var relacionAspecto = window.innerWidth/window.innerHeight;
 var planoCercano = 1;
-var planoLejano = 1000;
+var planoLejano = 600;
 
 var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
 
-camara.position.z = 200;
+camara.position.z = 150;
+camara.position.y = 20;
 
 var escena = new THREE.Scene();
 escena.add(cuadros);
