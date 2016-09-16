874a912fdb8fcb79a63ad12dd290cdce9b1e570b
Aleixein <megasaleixein@gmail.com>
2016-09-16 13:19:35 -0500

    Update ejercicio_6_tablero.js

--- rv/ejercicio_6_tablero.js
+++ rv/ejercicio_6_tablero.js
@@ -1,6 +1,3 @@
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 200;
-
 var cuboForma = new THREE.BoxGeometry(10,10,10)
 
 //var tableroForma = new THREE.Geometry();
@@ -20,11 +17,3 @@ for (var i = 0; i < 8; i++ ) {
     cuadros.add(item);
   }
 }
-
-var escena = new THREE.Scene();
-escena.add(cuadros);
-escena.add(torres);
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
