fbe91a6003ade1aeff13f16ce7d5db366faf5372
Aleixein <megasaleixein@gmail.com>
2016-09-16 15:58:01 -0500

    Update ejercicio_6_camaraOrtografica.js

--- rv/ejercicio_6_camaraOrtografica.js
+++ rv/ejercicio_6_camaraOrtografica.js
@@ -12,19 +12,11 @@ camara.position.z = 120;
 camara.position.y = 35;
 
 var escena = new THREE.Scene();
-
-var aristas = new THREE.EdgesHelper( marcoMalla, 0x474a4a );
-aristas.material.linewidth = 0.3;
-
-var aristas1 = new THREE.EdgesHelper( torres, 0x474a4a);
-aristas1.material.linewidth = 0.3;
-
 camara.lookAt(escena.position);
 escena.add(marcoMalla);
 escena.add(cuadros);
 escena.add(torres);
 escena.add( aristas );
-escena.add( aristas1 );
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderizador.domElement );
