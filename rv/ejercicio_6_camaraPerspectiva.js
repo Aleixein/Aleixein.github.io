609ec4a54e5c9f9070737e5a296d72154a42ebf0
Aleixein <megasaleixein@gmail.com>
2016-09-16 13:21:32 -0500

    Create ejercicio_6_camaraPerspectiva.js

---
+++ rv/ejercicio_6_camaraPerspectiva.js
@@ -0,0 +1,16 @@
+var campoVision = 45; //en grados
+var relacionAspecto = window.innerWidth/window.innerHeight;
+var planoCercano = 1;
+var planoLejano = 1000;
+
+var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
+
+camara.position.z = 200;
+
+var escena = new THREE.Scene();
+escena.add(cuadros);
+escena.add(torres);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
