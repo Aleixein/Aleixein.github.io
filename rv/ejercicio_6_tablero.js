466793f60562cd9dd96ac8e5910283578be91ea3
Aleixein <megasaleixein@gmail.com>
2016-09-16 14:01:38 -0500

    Update ejercicio_6_tablero.js

--- rv/ejercicio_6_tablero.js
+++ rv/ejercicio_6_tablero.js
@@ -17,3 +17,27 @@ for (var i = 0; i < 8; i++ ) {
     cuadros.add(item);
   }
 }
+
+var izqForma = new THREE.BoxGeometry(10,10,100);
+var derForma = new THREE.BoxGeometry(10,10,100);
+var abajoForma = new THREE.BoxGeometry(80,10,10);
+var arribaForma = new THREE.BoxGeometry(80,10,10);
+izq.translate(-10,0,35);
+der.translate(80,0,35);
+abajo.translate(35,0,80);
+arriba.translate(35,0,-10);
+
+var izqMalla = new THREE.Mesh(izqForma);
+var derMalla = new THREE.Mesh(derForma);
+var abajoMalla = new THREE.Mesh(abajoForma);
+var arribaMalla = new THRE.Mesh(arribaMalla);
+
+var marcoForma = new THREE.Geometry();
+
+marcoForma.merge( izqMalla.geometry, izqMalla.matrix );
+marcoForma.merge( derMalla.geometry, derMalla.matrix );
+marcoForma.merge( abajoMalla.geometry, abajoMalla.matrix );
+marcoForma.merge( arribaMalla.geometry, arribaMalla.matrix );
+
+var marcoMaterial = new THREE.MeshBasicMaterial( {color: '#895D05' });
+var marcoMalla = new THREE.Mesh( marcoForma, marcoMaterial );
