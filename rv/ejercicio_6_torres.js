f9a2c2845fee23cb4eabce769fa36053c4d4ab49
Aleixein <megasaleixein@gmail.com>
2016-09-16 15:57:19 -0500

    Update ejercicio_6_torres.js

--- rv/ejercicio_6_torres.js
+++ rv/ejercicio_6_torres.js
@@ -58,8 +58,6 @@ for ( var i = 0; i < 2; i++){
     item.position.x = i*70;
     item.position.z = j*70;
     item.position.y = 5;
-    aristas1 = new THREE.EdgesHelper(item,0x474a4a);
-    aristas1.material.lineWidth=0.3;
     torres.add(item);
   }
 }
