3ec95cc69a11f641684afbce44c9c1b0f5601aef
Aleixein <megasaleixein@gmail.com>
2016-09-16 14:33:55 -0500

    Update ejercicio_6_camaraOrtografica.js

--- rv/ejercicio_6_camaraOrtografica.js
+++ rv/ejercicio_6_camaraOrtografica.js
@@ -6,11 +6,11 @@ camara.top = window.innerHeight/2;
 camara.bottom = window.innerHeight/-2;
 camara.near = 0.1;
 camara.far = 600;
-//camara.updateProjectionMatrix();
+camara.updateProjectionMatrix();
 
-//camara.position.x = 35;
-camara.position.z = 160;
-//camara.position.y = 30;
+camara.position.x = 35;
+camara.position.z = 130;
+camara.position.y = 30;
 
 var escena = new THREE.Scene();
 escena.add(marcoMalla);
