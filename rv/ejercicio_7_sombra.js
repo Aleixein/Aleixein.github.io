45cfb768391193e186a54419792a7cb046549577
Aleixein <megasaleixein@gmail.com>
2016-09-20 15:31:39 -0500

    Update ejercicio_7_sombra.js

--- rv/ejercicio_7_sombra.js
+++ rv/ejercicio_7_sombra.js
@@ -1,1 +1,23 @@
 
+var escena = new THREE.Scene();
+escena.add(marcoMalla);
+escena.add(cuadros);
+escena.add(torres);
+
+var campoVision = 55; //en grados
+var relacionAspecto = window.innerWidth/window.innerHeight;
+var planoCercano = 1;
+var planoLejano = 600;
+
+var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano );
+
+camara.position.x = 100;
+camara.position.y = 100;
+camara.position.z = 100;
+
+camara.lookAt(escena.position);
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
