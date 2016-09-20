275765d181c7341bbad9bbb26c92d270ff1c311c
Aleixein <megasaleixein@gmail.com>
2016-09-20 15:50:31 -0500

    falta agregar las sombras

--- rv/ejercicio_7_sombra.js
+++ rv/ejercicio_7_sombra.js
@@ -1,8 +1,18 @@
+var luzMagenta = new THREE.PointLight(0xFF00FF);
+var luzCyan = new THREE.PointLight(0x00FFFF);
+var luzAmarilla = new THREE.PointLight(0xFFFF00);
+luzMagenta.position.x = 35;
+luzMagenta.position.y = 20;
+luzMagenta.position.z = 35;
+luzCyan.position.y = 25;
+luzAmarilla.position.x = 90;
+luzAmarilla.position.y = 22;
+luzAmarilla.position.z = 90;
 
 var escena = new THREE.Scene();
 escena.add(marcoMalla);
 escena.add(cuadros);
-//escena.add(torres);
+escena.add(torres);
 
 var campoVision = 55; //en grados
 var relacionAspecto = window.innerWidth/window.innerHeight;
@@ -15,9 +25,12 @@ camara.position.x = 100;
 camara.position.y = 100;
 camara.position.z = 100;
 
-camara.lookAt(escena.position);
+//camara.lookAt(escena.position);
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderizador.domElement);
+renderizador.shadowMapEnabled = true;
+
 renderizador.render(escena,camara);
+
