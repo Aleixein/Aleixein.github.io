decc87de261c979eece208185df68424ede227f0
Aleixein <megasaleixein@gmail.com>
2016-09-22 15:03:25 -0500

    Update ejemplo_funcion_1.js

--- ejemplo_funcion_1.js
+++ ejemplo_funcion_1.js
@@ -10,8 +10,13 @@ function init( p ) {
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700,700);
   document.body.appendChild( renderizador.domElement );
-}
+} //escena, camara y renderizador no tienen var porque son variables globales, se definen despues pero antes de
+  //la ejecución de las funciones
 
 var main = function(p){
   renderizador.render(escena,camara);
 }
+
+var escena, camara, renderizador;
+init(1);
+main(2);
