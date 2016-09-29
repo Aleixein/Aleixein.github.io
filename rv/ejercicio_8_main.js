60f50965fecbf4cd8b402afea09a7bb7a23f16f3
Aleixein <megasaleixein@gmail.com>
2016-09-29 15:39:37 -0500

    Create ejercicio_8_main.js

---
+++ rv/ejercicio_8_main.js
@@ -0,0 +1,37 @@
+TEXTURA.setup = function(){
+  var TEXTURA.iluminacion = new THREE.AmbientLight( 0xFFFFFF );
+  
+  TEXTURA.escena = new THREE.Scene();
+  TEXTURA.tablero();
+  TEXTURA.rooks();
+  TEXTURA.escena.add(TEXTURA.marcoMalla);
+  TEXTURA.escena.add(TEXTURA.cuadros);
+  TEXTURA.escena.add(TEXTURA.torres);
+  TEXTURA.escena.add(TEXTURA.iluminacion);
+  
+  var campoVision = 55; //en grados
+  var relacionAspecto = window.innerWidth/window.innerHeight;
+  var planoCercano = 1;
+  var planoLejano = 600;
+
+  TEXTURA.camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano);
+  TEXTURA.camara.position.x = 100;
+  TEXTURA.camara.position.y = 100;
+  TEXTURA.camara.position.z = 160;
+  TEXTURA.camara.lookAt(TEXTURA.escena.position);
+  
+  var lienzo = document.getElementById("tablero-textura");
+  TEXTURA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  TEXTURA.renderizador.setSize( window.innerWidth, window.innerHeight);
+}
+
+TEXTURA.loop = function(){
+  requestAnimationFrame(TEXTURA.loop);
+  
+  if( (TEXTURA.marcoMalla !== undefined) && (TEXTURA.cuadros !== undefined) && (Textura.torres !== undefined) )
+  
+  TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara );
+}
+
+TEXTURA.setup();
+TEXTURA.loop();
