4f7da681f3244eed4cf80ed1cc61a6992c1aa228
Aleixein <megasaleixein@gmail.com>
2016-09-28 23:05:59 -0500

    Update ejemplo-textura.js

--- ejemplo-textura.js
+++ ejemplo-textura.js
@@ -1,8 +1,8 @@
 var TEXTURA = new Object();
 
-TEXTURA.retrollamada = function( textura ) {
-  var material = new THREE.MeshBasicMaterial( {map: textura} );
-  TEXTURA.malla = new THREE.Mesh( new THREE.SphereGeometry(1,32,32), material );
+TEXTURA.retrollamada = function(textura){
+  var  material = new THREE.MeshBasicMaterial({map: textura}); //mapeo la imagen como material
+  TEXTURA.malla = new THREE.Mesh(new THREE.SphereGeometry(1), material);
   TEXTURA.escena.add(TEXTURA.malla);
 }
 
@@ -10,25 +10,26 @@ TEXTURA.setup = function(){
   TEXTURA.escena = new THREE.Scene();
   
   var cargador = new THREE.TextureLoader();
-  cargador.load("tierra.jpg", TEXTURA.retrollamada);                 
+  cargador.load("tierra.jpg", TEXTURA.retrollamada);
   
-  TEXTURA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth/Window.innerHeight, 0.1, 1000 );
+  TEXTURA.camara = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   
   TEXTURA.camara.position.z = 5;
   
   var lienzo = document.getElementById("ejemplo-textura");
-  TEXTURA.renderizador = new THREE.WebGLRenderer( {canvas: lienzo, antialias: true} );
-  TEXTURA.renderizador.setSize( 600, 600);
+  TEXTURA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  TEXTURA.renderizador.setSize(600, 600);
 }
 
-TEXTURA.loop = function() {
-  requestAnimationFrame( TEXTURA.loop );
+TEXTURA.loop = function(){
+  requestAnimationFrame(TEXTURA.loop);
   
-  if( TEXTURA.malla !== undefined ){
-    TEXTURA.malla.rotateX( 0.01 );
-    TEXTURA.malla.rotateY( 0.01 );
+  if (TEXTURA.malla !== undefined){
+    TEXTURA.malla.rotateX(0.01);
+    TEXTURA.malla.rotateY(0.01);
   }
-  TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara );
+  TEXTURA.renderizador.render(TEXTURA.escena, TEXTURA.camara);
+
 }
 
 TEXTURA.setup();
