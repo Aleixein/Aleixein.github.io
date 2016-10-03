02f809cab294a4e95eb26b910f9574204fe81c28
Aleixein <megasaleixein@gmail.com>
2016-10-03 18:28:30 -0500

    Update ejercicio_8_tablero.js

--- rv/ejercicio_8_tablero.js
+++ rv/ejercicio_8_tablero.js
@@ -1,20 +1,32 @@
 var TEXTURA = new Object();
-  
-TEXTURA.tablero = function(){
-  var cuboForma = new THREE.BoxGeometry(10,10,10);
-  TEXTURA.cuadros = new THREE.Object3D();
-  
-  var cargadorBlanco = new THREE.TextureLoader();
-  var cargadorNegro = new THREE.TextureLoader();
-  cargadorBlanco.load("marmol_blanco.jpg",TEXTURA.blanco);
-  cargadorNegro.load("marmol_negro.jpg",TEXTURA.negro);
+TEXTURA.cuadros = new THREE.Object3D();
 
+TEXTURA.cuadrosBlancos = function( textura ){
+  var marmolBlanco = new THREE.MeshLambertMaterial( {map: textura} ); 
+  //var cuboForma = new THREE.BoxGeometry(10,10,10);
+  //TEXTURA.cuadros = new THREE.Object3D();
+  
   for (var i = 0; i < 8; i++ ) {
     for ( var j = 0; j < 8; j++ ) {
       if ( (i+j) % 2 == 0){
         var item = new THREE.Mesh( cuboForma,marmolBlanco );
       }
-      else{
+      item.position.x = i*10;
+      item.position.z = j*10;
+      TEXTURA.cuadros.add(item);
+    }
+  }
+  TEXTURA.escena.add(TEXTURA.cuadros);
+}
+
+TEXTURA.cuadrosNegros = function( textura ){
+  var marmolNegro = new THREE.MeshLambertMaterial( {map: textura} );
+  //var cuboForma = new THREE.BoxGeometry(10,10,10);
+  //TEXTURA.cuadros = new THREE.Object3D();
+  
+  for (var i = 0; i < 8; i++ ) {
+    for ( var j = 0; j < 8; j++ ) {
+      if ( (i+j) % 2 == 1){        
         var item = new THREE.Mesh( cuboForma,marmolNegro );
       }
       item.position.x = i*10;
@@ -22,12 +34,11 @@ TEXTURA.tablero = function(){
       TEXTURA.cuadros.add(item);
     }
   }
+  TEXTURA.escena.add(TEXTURA.cuadros);
 }
 
-TEXTURA.base = function(){
-  var cargadorBase = new THREE.TextureLoader();
-  cargadorBase.load("madera.jpg",TEXTURA.madera);
-  
+TEXTURA.base = function( textura) {
+  var baseMadera = new THREE.MeshLambertMaterial( {map: textura} );
   var izqForma = new THREE.BoxGeometry(10,10,100);
   var derForma = new THREE.BoxGeometry(10,10,100);
   var abajoForma = new THREE.BoxGeometry(80,10,10);
@@ -50,4 +61,5 @@ TEXTURA.base = function(){
   marcoForma.merge( arribaMalla.geometry, arribaMalla.matrix );
   
   TEXTURA.marcoMalla = new THREE.Mesh( marcoForma, baseMadera );
+  TEXTURA.escena.add(TEXTURA.marcoMalla);
 }
