401b6fd9931581729083da140f1257158ffafcce
Aleixein <megasaleixein@gmail.com>
2016-10-03 18:34:53 -0500

    Update ejercicio_8_tablero.js

--- rv/ejercicio_8_tablero.js
+++ rv/ejercicio_8_tablero.js
@@ -63,3 +63,42 @@ TEXTURA.base = function( textura) {
   TEXTURA.marcoMalla = new THREE.Mesh( marcoForma, baseMadera );
   TEXTURA.escena.add(TEXTURA.marcoMalla);
 }
+
+TEXTURA.setup = function(){
+  TEXTURA.iluminacion = new THREE.AmbientLight( 0xFFFFFF );
+
+  TEXTURA.escena = new THREE.Scene(); 
+  TEXTURA.escena.add(TEXTURA.iluminacion);
+  
+  var cargadorCuadrosB = new THREE.TextureLoader();
+  cargardorCuadrosB.load("marmol_blanco.jpg",TEXTURA.cuadrosBlancos);
+  
+  var cargadorCuadrosN = new THREE.TextureLoader();
+  cargardorCuadrosN.load("marmol_negro.jpg",TEXTURA.cuadrosNegros);
+  
+  var cargadorBase = new THREE.TextureLoader();
+  cargardorBase.load("madera.jpg",TEXTURA.base);
+  
+  var campoVision = 55; //en grados
+  var relacionAspecto = window.innerWidth/window.innerHeight;
+  var planoCercano = 1;
+  var planoLejano = 600;
+
+  TEXTURA.camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano);
+  TEXTURA.camara.position.x = 100;
+  TEXTURA.camara.position.y = 100;
+  TEXTURA.camara.position.z = 160;
+  TEXTURA.camara.lookAt(TEXTURA.escena.position);
+  
+  var lienzo = document.getElementById("tablero-textura");
+  TEXTURA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  TEXTURA.renderizador.setSize( window.innerWidth, window.innerHeight);
+}
+
+TEXTURA.loop = function(){
+  requestAnimationFrame(TEXTURA.loop);
+  
+  if( (TEXTURA.marcoMalla !== undefined) && (TEXTURA.cuadros !== undefined) )
+  
+  TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara );
+}
