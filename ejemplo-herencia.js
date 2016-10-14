868003b6ca3fc0060926504e7f2e0cf667aeb13f
Aleixein <megasaleixein@gmail.com>
2016-10-14 11:06:44 -0500

    Update ejemplo-herencia.js

--- ejemplo-herencia.js
+++ ejemplo-herencia.js
@@ -26,9 +26,9 @@ PROTOTIPO.setup = function(){
   PROTOTIPO.camara = new THREE.PerspectiveCamera();
   PROTOTIPO.camara.position.z = 20;
   
-  var lienzo = document.getElementById("ejemplo-prototipo");
-  PROTOTIPO.renderizador = new THREE.WebGLRenderer( { canvas: lienzo, antialias: true } );
+  PROTOTIPO.renderizador = new THREE.WebGLRenderer( { antialias: true } );
   PROTOTIPO.renderizador.setSize(600,600);
+  document.body.appendChild(PROTOTIPO.renderizador.domElement);
   
   PROTOTIPO.escena = new THREE.Scene();
   PROTOTIPO.escena.add(arbol1);
