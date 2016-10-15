31cc2db95fc7b190810f4f364b6b15ef44af1542
Aleixein <megasaleixein@gmail.com>
2016-10-15 02:21:18 -0500

    Update ejercicio_8_main.js

--- rv/ejercicio_8_main.js
+++ rv/ejercicio_8_main.js
@@ -32,7 +32,7 @@ AJEDREZ.setup = function(){
 AJEDREZ.loop = function(){
   requestAnimationFrame(AJEDREZ.loop);
   
-  if( (AJEDREZ.marcoMalla !== undefined) && (AJEDREZ.cuadrosB !== undefined) && (AJEDREZ.cuadrosN !== undefined))
+  if( (AJEDREZ.marcoMalla !== undefined) && (AJEDREZ.cuadros !== undefined) )
   
   AJEDREZ.renderizador.render( AJEDREZ.escena, AJEDREZ.camara );
 }
