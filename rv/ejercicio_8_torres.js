b9d1c252bc9e45c16b2ec9ff2e02655e1bc561e5
Aleixein <megasaleixein@gmail.com>
2016-10-17 15:53:30 -0500

    Update ejercicio_8_torres.js

--- rv/ejercicio_8_torres.js
+++ rv/ejercicio_8_torres.js
@@ -51,10 +51,10 @@ AJEDREZ.torreForma = function(){
 AJEDREZ.torreForma.prototype = new THREE.Geometry();
 
 AJEDREZ.torres = function(){
-  var torreBlanca1 = new THREE.Mesh( new AJEDREZ.torreForma(), AJEDREZ.marmolB);
-  var torreBlanca2 = new THREE.Mesh( new AJEDREZ.torreForma(), AJEDREZ.marmolB);
-  var torreNegra1 = new THREE.Mesh( new AJEDREZ.torreForma(), AJEDREZ.marmolN);
-  var torreNegra2 = new THREE.Mesh( new AJEDREZ.torreForma(), AJEDREZ.marmolN);
+  var torreBlanca1 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolB);
+  var torreBlanca2 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolB);
+  var torreNegra1 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolN);
+  var torreNegra2 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolN);
   torreBlanca1.position.y = 5;
   torreBlanca2.translate(70,5,70);
   torreNegra1.translate(0,5,70);
