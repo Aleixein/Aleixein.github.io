70fb348510b70f3f59aa1484cfb480c26ee8680f
Aleixein <megasaleixein@gmail.com>
2016-10-17 15:58:31 -0500

    Update ejercicio_8_main.js

--- rv/ejercicio_8_main.js
+++ rv/ejercicio_8_main.js
@@ -4,27 +4,14 @@ AJEDREZ.setup = function(){
   AJEDREZ.escena = new THREE.Scene(); 
   AJEDREZ.escena.add(AJEDREZ.iluminacion);
   
-  //var cargadorCuadrosB = new THREE.TextureLoader();
- // cargadorCuadrosB.load("marmol_blanco.jpg",AJEDREZ.marmolBlanco);
+  var cargadorColorB = new THREE.TextureLoader();
+  cargadorColorB.load("marmol_blanco.jpg",AJEDREZ.marmolBlanco);
   
- // var cargadorCuadrosN = new THREE.TextureLoader();
-//  cargadorCuadrosN.load("marmol_negro.jpg",AJEDREZ.marmolNegro);
+  var cargadorColorN = new THREE.TextureLoader();
+  cargadorColorN.load("marmol_negro.jpg",AJEDREZ.marmolNegro);
   AJEDREZ.tablero();
   var cargadorBase = new THREE.TextureLoader();
   cargadorBase.load("madera.JPG",AJEDREZ.base);
-  
-  var torreBlanca1 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolB);
-  var torreBlanca2 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolB);
-  var torreNegra1 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolN);
-  var torreNegra2 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolN);
-  torreBlanca1.position.y = 5;
-  torreBlanca2.translate(70,5,70);
-  torreNegra1.translate(0,5,70);
-  torreNegra2.translate(70,5,0);
-  AJEDREZ.escena.add(torreBlanca1);
-  AJEDREZ.escena.add(torreBlanca2);
-  AJEDREZ.escena.add(torreNegra1);
-  AJEDREZ.escena.add(torreNegra2);
   
   var campoVision = 55; //en grados
   var relacionAspecto = window.innerWidth/window.innerHeight;
