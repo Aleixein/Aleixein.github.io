d0b6c444ab3af4cb438f119e30a1d4ccdf46fb64
Aleixein <megasaleixein@gmail.com>
2016-10-17 15:37:47 -0500

    Update ejercicio_8_torres.js

--- rv/ejercicio_8_torres.js
+++ rv/ejercicio_8_torres.js
@@ -1,4 +1,6 @@
-function torre(){ 
+AJEDREZ.torreForma = function(){ 
+  
+  THREE.Geometry.call(this);
   
   var base1Forma = new THREE.CylinderGeometry( 3.5, 3.5, 1, 20);
   var base2Forma = new THREE.CylinderGeometry( 3, 3, 1, 20);
@@ -32,17 +34,33 @@ function torre(){
   var diente3Malla = new THREE.Mesh(diente3Forma);
   var diente4Malla = new THREE.Mesh(diente4Forma);
 
-  this.Forma = new THREE.Geometry();
 
-  this.Forma.merge(base1Malla.geometry, base1Malla.matrix);
-  this.Forma.merge(base2Malla.geometry, base2Malla.matrix);
-  this.Forma.merge(base3Malla.geometry, base3Malla.matrix);
-  this.Forma.merge(base4Malla.geometry, base4Malla.matrix);
-  this.Forma.merge(base5Malla.geometry, base5Malla.matrix);
-  this.Forma.merge( cimaMalla.geometry, cimaMalla.matrix );
-  this.Forma.merge(diente1Malla.geometry, diente1Malla.matrix);
-  this.Forma.merge(diente2Malla.geometry, diente2Malla.matrix);
-  this.Forma.merge(diente3Malla.geometry, diente3Malla.matrix);
-  this.Forma.merge(diente4Malla.geometry, diente4Malla.matrix);
+  this.merge(base1Malla.geometry, base1Malla.matrix);
+  this.merge(base2Malla.geometry, base2Malla.matrix);
+  this.merge(base3Malla.geometry, base3Malla.matrix);
+  this.merge(base4Malla.geometry, base4Malla.matrix);
+  this.merge(base5Malla.geometry, base5Malla.matrix);
+  this.merge( cimaMalla.geometry, cimaMalla.matrix );
+  this.merge(diente1Malla.geometry, diente1Malla.matrix);
+  this.merge(diente2Malla.geometry, diente2Malla.matrix);
+  this.merge(diente3Malla.geometry, diente3Malla.matrix);
+  this.merge(diente4Malla.geometry, diente4Malla.matrix);
  
+}
+
+AJEDREZ.torreForma.prototype = new THREE.Geometry();
+
+AJEDREZ.torres = function(){
+  var torreBlanca1 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolBlanco);
+  var torreBlanca2 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolBlanco);
+  var torreNegra1 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolNegro);
+  var torreNegra2 = new THREE.Mesh( new AJEDREZ.torreForma(), marmolNegro);
+  torreBlanca1.position.y = 5;
+  torreBlanca2.translate(70,5,70);
+  torreNegra1.translate(0,5,70);
+  torreNegra2.translate(70,5,0);
+  AJEDREZ.escena.add(torreBlanca1);
+  AJEDREZ.escena.add(torreBlanca2);
+  AJEDREZ.escena.add(torreNegra1);
+  AJEDREZ.escena.add(torreNegra2);
 }
