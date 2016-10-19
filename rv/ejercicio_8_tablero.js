98eb9f9857650158a11b6b25732dbb819c398a5b
Aleixein <megasaleixein@gmail.com>
2016-10-19 17:57:34 -0500

    Update ejercicio_8_tablero.js

--- rv/ejercicio_8_tablero.js
+++ rv/ejercicio_8_tablero.js
@@ -5,7 +5,7 @@ var mat3=false;
 
 AJEDREZ.cuadros = new THREE.Object3D();
 
-AJEDREZ.tablero = function(){
+/*AJEDREZ.tablero = function(){
  
   var cuboForma = new THREE.BoxGeometry(10,10,10);
   var cargadorCuadrosB = new THREE.TextureLoader();
@@ -29,7 +29,7 @@ AJEDREZ.tablero = function(){
     }
   }
   AJEDREZ.escena.add(AJEDREZ.cuadros);
-}
+}*/
 
 AJEDREZ.base = function( textura) {
   var baseMadera = new THREE.MeshLambertMaterial( {map: textura} );
@@ -57,4 +57,58 @@ AJEDREZ.base = function( textura) {
   
   AJEDREZ.marcoMalla = new THREE.Mesh( marcoForma, baseMadera );
   AJEDREZ.escena.add(AJEDREZ.marcoMalla);  
+}
+
+AJEDREZ.marmolBlanco = function(textura){
+  var colorBlanco = new THREE.MeshLambertMaterial( {map: textura} );
+  mat1 = true;
+  AJEDREZ.torreBlanca1 = new THREE.Mesh( new AJEDREZ.torreForma(), colorBlanco);
+  AJEDREZ.torreBlanca2 = new THREE.Mesh( new AJEDREZ.torreForma(), colorBlanco);
+  AJEDREZ.torreBlanca1.position.y = 5;
+  AJEDREZ.torreBlanca2.position.x = 70;
+  AJEDREZ.torreBlanca2.position.y = 5;
+  AJEDREZ.torreBlanca2.position.z = 70;
+  AJEDREZ.escena.add(AJEDREZ.torreBlanca1);
+  AJEDREZ.escena.add(AJEDREZ.torreBlanca2);
+ 
+  var cuboForma = new THREE.BoxGeometry(10,10,10);
+  
+  for (var i = 0; i < 8; i++ ) {
+    for ( var j = 0; j < 8; j++ ) {
+      if ( (i+j) % 2 == 0){                    
+        var item = new THREE.Mesh( cuboForma,colorBlanco );
+      }     
+      item.position.x = i*10;
+      item.position.z = j*10;
+      AJEDREZ.cuadros.add(item);
+    }
+  }
+  AJEDREZ.escena.add(AJEDREZ.cuadros);
+}
+
+AJEDREZ.marmolNegro = function(textura){
+  var colorNegro = new THREE.MeshLambertMaterial( {map: textura} );
+  mat2 = true;
+  AJEDREZ.torreNegra1 = new THREE.Mesh( new AJEDREZ.torreForma(), colorNegro);
+  AJEDREZ.torreNegra2 = new THREE.Mesh( new AJEDREZ.torreForma(), colorNegro);
+  AJEDREZ.torreNegra1.position.y = 5;
+  AJEDREZ.torreNegra1.position.z = 70;
+  AJEDREZ.torreNegra2.position.x = 70;
+  AJEDREZ.torreNegra2.position.y = 5;
+  AJEDREZ.escena.add(AJEDREZ.torreNegra1);
+  AJEDREZ.escena.add(AJEDREZ.torreNegra2);
+ 
+  var cuboForma = new THREE.BoxGeometry(10,10,10);
+  
+  for (var i = 0; i < 8; i++ ) {
+    for ( var j = 0; j < 8; j++ ) {
+      if ( (i+j) % 2 !== 0){                    
+        var item = new THREE.Mesh( cuboForma,colorNegro );
+      }
+      item.position.x = i*10;
+      item.position.z = j*10;
+      AJEDREZ.cuadros.add(item);
+    }
+  }
+  AJEDREZ.escena.add(AJEDREZ.cuadros);
 }
