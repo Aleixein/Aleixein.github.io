e3b2173a6291721e9531dd06d263de6419b61d74
Aleixein <megasaleixein@gmail.com>
2016-10-19 17:49:31 -0500

    Update ejercicio_8_main.js

--- rv/ejercicio_8_main.js
+++ rv/ejercicio_8_main.js
@@ -29,18 +29,14 @@ AJEDREZ.setup = function(){
   AJEDREZ.renderizador.setSize( window.innerWidth, window.innerHeight);
 }
 
-var didSetup = 1;
 
 AJEDREZ.loop = function(){
   requestAnimationFrame(AJEDREZ.loop);
   
   if( mat1 && mat2 && mat3 ){
-    if (didSetup == 1){
-      AJEDREZ.setup();
-      didSetup = 0;
-    }
     AJEDREZ.renderizador.render( AJEDREZ.escena, AJEDREZ.camara );
   }
 }
 
+AJEDREZ.setup();
 AJEDREZ.loop();
